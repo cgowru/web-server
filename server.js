d6b24356b392de2a2e8860ab3fc2df23fbc9de6b
Chandra Sekhar <cgowru@gmail.com>
2016-01-06 22:37:31 +0530

    Update logger

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var middleware = {
 		next();
 	},
 	logger:function(req,res,next) {
-		console.log('Request @'+new Date().toString()+' : '+req.method +' '+req.originalUrl);
+		console.log('Request '+new Date().toString()+' : '+req.method +' '+req.originalUrl);
 		next();
 	}
 }
