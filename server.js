34c4906e4957e5c06cfaabad759de36936b49a4d
unknown <cgowru@gmail.com>
2018-09-13 14:36:25 +0530

    Chnged env port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3100;
 var middleware = require('./middleware');
 
 app.get('/',function(req,res){
-	res.send('Hello Express!!!!!!!!');
+	res.send('Hello Express!');
 });
 
 app.use(middleware.logger);
